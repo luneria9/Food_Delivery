08d0e3817dcd457539c846cd6280bd112c1a3684
Patricia Bourjeily <patriciabourjeily@gmail.com>
2025-08-24 20:04:55 -0400

    Removed my persoanl credentials

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-await mongoose.connect("mongodb+srv://luneria444:56789012@cluster0.6q62bcb.mongodb.net/food-del").then(() => console.log("DB Connected"));
+await mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.6q62bcb.mongodb.net/food-del`).then(() => console.log("DB Connected"));
 }
